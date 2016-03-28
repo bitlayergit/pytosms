86854d22cb2abc27a66dbebc05b57407cd8ec475
Brunston Poon <brupoon@outlook.com>
2016-03-28 00:27:22 -0400

    support for alternate carriers, license

--- pytosms.py
+++ pytosms.py
@@ -1,11 +1,9 @@
-#pytosms.py#
-#(c) Brunston Poon 2016
-#maintained by github/brupoon
+# pytosms
+# (c) Brunston Poon 2016
+# MIT License
 
 import smtplib
 from email.mime.text import MIMEText
-import random
-import string
 
 class SMS(object):
     """
@@ -17,13 +15,25 @@ class SMS(object):
         msg: a string containing the message (not to be longer than 120 chars)
     """
     
-    def __init__(self, p_to, p_from, msg):
+    def __init__(self, to, fr, msg, sendhost):
         """Return an SMS object"""
-        self.p_to = p_to
-        self.p_from = p_from
+        self.to = to
+        self.fr = fr
         self.msg = msg
-    
+        #generic domain if no sending domain specified (via "")
+        if sendhost == "":
+            self.sendhost = "@example.org"
+        #this gets appended to our "from" phone number
+        else:
+            self.sendhost = "@"
+            self.sendhost += sendhost
+
+
     def send(self):
+        """
+        Sends message. Returns True if successfully used SMTP server to send message,
+        returns False if there is no server running.
+        """
         text_to_send = MIMEText(self.msg)
 
         carriers = ["@txt.att.net", "@txt.windmobile.ca", "@vxtext.com",\
@@ -32,14 +42,22 @@ class SMS(object):
                     "@mobile.celloneusa.com", "@msg.telus.com", "paging.acswireless.com",\
                     "@pcs.rogers.com", "@questmp.com", "@sms.mycricket.com",\
                     "@sms.ntwls.net","@tmomail.net"]
-
-        self.p_to += "@txt.att.net"
-
         
-        self.p_from += "@example.com"
-        
-        s = smtplib.SMTP('localhost')
-        #s.send_message(text_to_send)
-        s.sendmail(self.p_from, self.p_to, self.msg)
-        s.quit()
+        #append the sending host to our from address
+        fr_address = self.fr
+        fr_address += self.sendhost
 
+        for carrier in carriers:
+            to_address = self.to
+            to_address += carrier
+            
+            try:        
+                s = smtplib.SMTP('localhost')
+                #s.send_message(text_to_send)
+                s.sendmail(fr_addess, to_address, self.msg)
+                s.quit()
+            except ConnectionRefusedError:
+                print("You are not running an SMTP server, i.e. sendmail, on your local machine. Exiting.")
+                return False
+
+        return True
